92bb1a985eb8d99faa9988557897cd10bd35dc90
ashwinijawadwar <a@j.com>
2025-01-15 18:18:04 +0530

    something

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('abb maja ayega na bhiduuu');
+ res.send('abb maja ayega na bhiduuu aur jetha');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
